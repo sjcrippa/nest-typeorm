60188feda944c9a610a83d9bf12748d09ca74e22
Santiago Crippa <santicrippa@gmail.com>
2024-09-13 12:01:49 -0300

    Connecting to mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      username: 'root',
+      password: 'sjcrippadb',
+      host: 'localhost',
+      port: 3306,
+      database: 'nestdb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // sincroniza las clases con la tabla en desarrollo, en prod usar una migracion.
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
