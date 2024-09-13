bf1c00d460810f18b781481f3c1c7c365eca5053
Santiago Crippa <santicrippa@gmail.com>
2024-09-13 15:04:33 -0300

    Getting users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService) {} // making the instance
+  constructor(private userService: UsersService) {} // connecting with the services
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.userService.getUsers();
+  }
 
   @Post()
-  createUser(@Body() newUser: CreateUserDto) {
+  createUser(@Body() newUser: CreateUserDto): Promise<User> {
     return this.userService.createUser(newUser);
   }
 }
