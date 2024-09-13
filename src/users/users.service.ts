a8615d4793dbe0a56b5f1394b64e6ff6cca96d4b
Santiago Crippa <santicrippa@gmail.com>
2024-09-13 12:51:07 -0300

    Creating users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {} // service connected to typeorm entity
+
+  createUser(user: CreateUserDto) {
+    const newUser = this.userRepository.create(user);
+    return this.userRepository.save(newUser);
+  }
+}
